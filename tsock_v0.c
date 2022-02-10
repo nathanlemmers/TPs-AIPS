4cafc165eee627d499e80e3fd2b48260c6919ea3
nathanlemmers <99388999+nathanlemmers@users.noreply.github.com>
2022-02-10 10:47:40 +0100

    Update tsock_v0.c

--- tsock_v0.c
+++ tsock_v0.c
@@ -18,15 +18,48 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
+
+void construire_message(char *message, char motif, int lg) {
+    int i;
+    for (i=0;i<lg;i++) message[i] = motif;
+}
+
+
+void afficher_message(char *message, int lg) {
+    int i;
+    printf("message construit : ");
+    for (i=0;i<lg;i++) printf("%c", message[i]);
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
 void main (int argc, char **argv)
 {
+    int sock;
+    int port=atoi(argv[argc-1]);
+    char * message=malloc(30*sizeof(char));
 	int c;
 	extern char *optarg;
 	extern int optind;
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
-	while ((c = getopt(argc, argv, "pn:s")) != -1) {
+	int protocole=0; /* 0=TCP 1=UDP */
+	int lg=30 ;/* longueur du message*/
+	while ((c = getopt(argc, argv, "pn:sul:")) != -1) {
 		switch (c) {
+		case 'l':
+		    lg=atoi(optarg);
+		    exit(1);
+		case 'u':
+		    protocole=1;
+		    break;
 		case 'p':
 			if (source == 1) {
 				printf("usage: cmd [-p|-s][-n ##]\n");
@@ -76,5 +109,42 @@ void main (int argc, char **argv)
 		printf("nb de tampons à envoyer = infini\n");
 
 	}
+	if (protocole==0){
+	    printf("Protocole TCP\n");
+    }
+	else{
+	    printf("Protocole UDP\n");
+	}
+	if (protocole==1){
+	    if (source==1){
+	        struct hostent *hp;
+	        struct sockaddr_in adr_distant;
+	        if((sock=socket(AF_INET,SOCK_DGRAM,0))==-1){
+	            printf("échec de création du socket\n");
+	            exit(1);
+	        }
+	        memset((char *)& adr_distant, 0, sizeof(adr_distant)) ;
+            adr_distant.sin_family = AF_INET ; /* domaine Internet*/
+            adr_distant.sin_port = htons(port) ;  
+            
+            /* Adresse IP */
+	        if((hp=gethostbyname(argv[argc-2]))==NULL){
+	            printf("erreur gethostbyname\n") ;
+	            exit(1) ;
+	        }   
+	        memcpy( (char*)&(adr_distant.sin_addr.s_addr),hp->h_addr,hp->h_length ); 
+            for(int i=0; i<nb_message+1; i++){
+                sprintf(message,"%5d",i+1);
+                construire_message(message+5,(char)(i%26)+97, lg-5);
+                if(sendto(sock,message,lg,0,(struct sockaddr_in *)&adr_distant, sizeof(adr_distant))==-1){
+                    printf("erreur sendto\n");
+                    exit(1);
+                }
+                printf("SOURCE: Envoie n°%d (%d) [%s]\n",i+1,lg, message);
+                
+            }
+       } 
+        /*if(source==0){
+            if((sock=socket(AF_INET,SOCK_DGRAM,0))==-1*/
+    }
 }
-
